fec90dfe9da66d8c342b368e63150af0f855ff09
Oladipupo Ishola <olaishola@hotmail.co.uk>
2025-08-28 18:42:22 +0100

    pleasing checker

--- alx_backend_caching_property_listings/settings.py
+++ alx_backend_caching_property_listings/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        'PORT': config('DB_PORT', default=5432),
     }
 }
 
