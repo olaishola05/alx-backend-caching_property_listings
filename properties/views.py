df9797b681e8a132f817f02d57da550aa978086e
Oladipupo Ishola <olaishola@hotmail.co.uk>
2025-08-28 18:55:18 +0100

    Update to return json data

--- properties/views.py
+++ properties/views.py
@@ -2,16 +2,28 @@ from django.shortcuts import render
 from django.core.cache import cache
 from django.views.decorators.cache import cache_page
 from .models import Property
+from django.http import JsonResponse
+from django.forms.models import model_to_dict
 
 @cache_page(60 * 15)
 def property_list(request):
     """
     Returns a list of all properties, with the response cached in Redis for 15 minutes.
     """
-    # This code will only execute if the page is not in the cache or has expired.
+    cached_response = cache.get('property_list')
+    if cached_response:
+        return cached_response
+   
     properties = Property.objects.all().order_by('created_at')
+    properties_list = [model_to_dict(prop) for prop in properties]
+
+    response = JsonResponse({
+        'success': True,
+        'count': len(properties_list),
+        'properties': properties_list
+    }, status=200)
+
     
-    context = {
-        'properties': properties
-    }
-    return render(request, 'properties/property_list.html', context)
+    cache.set('property_list', response, timeout=60 * 15)
+
+    return response
